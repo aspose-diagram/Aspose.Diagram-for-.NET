765421264b374014b59ddf7308099f0d6a5206bd
imran.rafique <imran.rafique@aspose.com>
2016-02-29 11:55:58 +0500

    Create a new diagram example

---
+++ Examples/CSharp/ProgrammersGuide/Load-Save-Convert/CreateNewVisio.cs
@@ -0,0 +1,26 @@
+﻿using Aspose.Diagram;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.ProgrammersGuide.Load_Save_Convert
+{
+    public class CreateNewVisio
+    {
+        public static void Run()
+        {
+            //ExStart:CreateNewVisio
+
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_LoadSaveConvert();
+
+            // initialize a Diagram class
+            Diagram diagram = new Diagram();
+
+            // save diagram in the VSDX format
+            diagram.Save(dataDir + "MyDiagram.vsdx", SaveFileFormat.VSDX);
+            //ExEnd:CreateNewVisio
+        }
+    }
+}
