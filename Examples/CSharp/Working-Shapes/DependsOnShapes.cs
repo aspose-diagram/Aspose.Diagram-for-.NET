c4d74a8a918d2d3b2a05f38b8a297fa86c6889b7
Philip Zhou <philip.zhou@aspose.com>
2021-09-30 16:51:07 +0800

    Examples updated to Aspose.Diagram for .NET 21.9

---
+++ Examples/CSharp/Working-Shapes/DependsOnShapes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Diagram.Examples.CSharp.Working_Shapes
+{
+    public class DependsOnShapes
+    {
+        public static void Run()
+        {
+            // ExStart:DependsOnShapes
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Shapes();
+
+            // Call a Diagram class constructor to load the VSDX diagram
+            Diagram diagram = new Diagram(dataDir + "Drawing1.vsdx");
+            // Get page by name
+            Page page = diagram.Pages.GetPage("Page-3");
+
+            foreach (Aspose.Diagram.Shape shape in page.Shapes)
+            {
+	            long[] shapeids = shape.DependsOnShapes();
+	            int count = 0;
+	            Console.WriteLine("Depends On");
+	            foreach (long id in shapeids)
+	            {
+	                count++;
+	                Console.WriteLine("\t" + count + ")\tID: " + id);
+	            }
+            }
+            // ExEnd:DependsOnShapes
+        }
+    }
+}
---
+++ Examples/CSharp/Working-Shapes/InheritTextBlock.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aspose.Diagram.Examples.CSharp.Working_Shapes
+{
+    public class InheritTextBlock
+    {
+        public static void Run()
+        {
+            // ExStart:InheritTextBlock
+            // The path to the documents directory.
+            string dataDir = RunExamples.GetDataDir_Shapes();
+
+            // Call a Diagram class constructor to load the VSDX diagram
+            Diagram diagram = new Diagram(dataDir + "Drawing1.vsdx");
+            // Get page by name
+            Page page = diagram.Pages.GetPage("Page-3");
+
+            foreach (Aspose.Diagram.Shape shape in page.Shapes)
+            {
+		            Aspose.Diagram.TextBlock textblock = shape.InheritTextBlock;
+		            Console.WriteLine(textblock.TopMargin.Value);
+		            Console.WriteLine(textblock.BottomMargin.Value);
+		            Console.WriteLine(textblock.RightMargin.Value);
+		            Console.WriteLine(textblock.LeftMargin.Value);
+		            Console.WriteLine(textblock.TextDirection.Value);
+            }
+            // ExEnd:InheritTextBlock
+        }
+    }
+}
